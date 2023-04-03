655186b0f291a0c906d0b2fcb94175536757d10d
manelmendez <maneeel93@gmail.com>
2023-04-03 18:16:49 +0200

    server config for Caddy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 // solo lo he creado para cambiar el puerto
-
 import { defineConfig } from "vite";
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig ({
+  plugins: [reactRefresh()],
   server: {
-    port: 5004
+    host: true,
+    port: 5004,
+    hmr: {
+      host: 'localhost',
+      protocol: 'wss',
+      clientPort: 443
+    }
   },
   preview: {
     port: 5005
